5b869100ceb657c6658973fa297ee2caa743974b
biguokang <biguokang@vtron.com>
2020-10-15 15:37:09 +0800

    add Alpaca AI

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const inquirer = require("inquirer");
 const clear = require("clear");
 const open = require("open");
 const qrcode = require('qrcode-terminal');
+const request = require('request');
+
 
 clear();
 
@@ -38,10 +40,12 @@ lmap.set("english",{
     answer1:"\nDone, see you soon at inbox.\n",
     question2:`-Contact me through ${chalk.green.bold("WeChat")}?`,
     answer2:"Scan the wechat qrcode to add my WeChat!!",
-    question3:`-${chalk.yellow.bold("banana dance!!!")}`,
+    question3:`-${chalk.yellow.bold("Banana dance!!!")}`,
     answer3:"\nbanana!!!!\n",
     question4:"-Just quit.",
-    answer4:"\nBye!!!!\n"
+    answer4:"\nBye!!!!\n",
+
+    alpacaAIMessage:`-Chat with ${chalk.hex("#f90").bold("Alpaca AI")}(sorry, only support Chinese)`
 })
 
 lmap.set("chinese",{
@@ -68,7 +72,9 @@ lmap.set("chinese",{
     question3:`-${chalk.yellow.bold("看香蕉君跳舞")}`,
     answer3:"\n气氛逐渐蕉♂灼♂\n",
     question4:"-退出",
-    answer4:"\n走好不送\n"
+    answer4:"\n走好不送\n",
+
+    alpacaAIMessage:`-和${chalk.hex("#f90").bold("Alpaca AI")}聊天`
 })
 
 const questions1 = [
@@ -120,6 +126,37 @@ prompt(questions1).then(answer => {
 
     console.log(info);
 
+
+    const alpacaAI = [
+        {
+            type: 'input',
+            message: '你:',
+            name: 'phone',
+            validate: function(val) {
+                if(val == "exit"){
+                    console.log(`\n${chalk.hex("#f90").bold("Alpaca AI")}已退出`);
+                    return true
+                }else{
+             
+                    request.post({
+                        url:'https://alpaca.run/apis/ai/text', 
+                        form:{
+                            "textType": "npx",
+                            "text": val
+                        }
+                    }, function(error, response) {
+                        let resText = JSON.parse(response.body).message
+                        console.log(`\n${chalk.hex("#f90").bold("Alpaca AI")}: ${resText}`);
+                    })
+
+                }
+                
+            }
+        }
+    ];
+
+    
+
     const questions2 = [
         {
             type: "list",
@@ -127,10 +164,39 @@ prompt(questions1).then(answer => {
             message: lmap.get(language).questionsMessage,
             choices: [
                 {
+                    name: lmap.get(language).alpacaAIMessage,
+                    value: () => {
+                        setTimeout(()=>{
+                            console.log(`${new Date().toLocaleString( )} 已成功连入Alpaca AI服务器,输入${chalk.red.bold("exit")}即可退出`);
+                            setTimeout(()=>{
+                                console.log(`${chalk.hex("#f90").bold("Alpaca AI")}: 你好啊，我是Alpaca AI,一个人工智能，你可以打字和我聊天！！！！`);
+                                setTimeout(()=>{
+                                    console.log(`${chalk.hex("#f90").bold("Alpaca AI")}: 我带有脏话检测功能，所以请文明发言哦！！！！`);
+                                    prompt(alpacaAI).then(() => {
+                                        setTimeout(()=>{
+                                            clear()
+                                            console.log("\n\n\n")
+                                            console.log(info);
+                                            prompt(questions2).then(answer => answer.action());
+                                        },1500)
+                                    })
+                                },1000)
+                            },1000)
+                        },1000)
+                        
+                        
+                    }
+                },
+                {
                     name: lmap.get(language).question1,
                     value: () => {
                         open("mailto:biguokang@outlook.com");
                         console.log(lmap.get(language).answer1);
+                        setTimeout(()=>{
+                            console.log("\n\n\n")
+                            console.log(info);
+                            prompt(questions2).then(answer => answer.action());
+                        },1500)
                     }
                 },
                 {
@@ -139,6 +205,11 @@ prompt(questions1).then(answer => {
                         console.log(lmap.get(language).answer2)
                         const url = 'https://u.wechat.com/MI8g1d4fSdEntqOdCrp-DU8';
                         qrcode.generate(url,{small:true});
+                        setTimeout(()=>{
+                            console.log("\n\n\n")
+                            console.log(info);
+                            prompt(questions2).then(answer => answer.action());
+                        },1500)
                     }
                 },
                 {
@@ -146,13 +217,18 @@ prompt(questions1).then(answer => {
                     value: () => {
                         console.log(lmap.get(language).answer3);
                         open("https://cdn.alpaca.run/js/banana.html")
+                        setTimeout(()=>{
+                            console.log("\n\n\n")
+                            console.log(info);
+                            prompt(questions2).then(answer => answer.action());
+                        },1500)
                     }
                 },
                 {
                     name: lmap.get(language).question4,
                     value: () => {
                         console.log(lmap.get(language).answer4);
-                        open("https://cdn.alpaca.run/default/ph.jpg")
+                        //open("https://cdn.alpaca.run/default/ph.jpg")
                     }
                 }
             ]
